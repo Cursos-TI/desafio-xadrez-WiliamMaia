b301c19b81fa0974f3bfce9cdd62c60a648c6155
WiliamMaia <wiliam.maia.silva@gmail.com>
2025-03-10 12:43:21 +0000

    # Apliqui as movimentações das 3 peças (Torre, Bispo e Rainha) conforme o instruído a não ser pela escolha de quantas casas se movimentar para a direção apontada ... Me segurei muito para não aplicar 'if-else' e mais 'switches' para oferecer todas as direções a essas peças e até mesmo acrescentar asoutras que faltam ...vamos devagar e com calma

--- xadrez.c
+++ xadrez.c
@@ -5,17 +5,41 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    int torre, bispo, rainha, escolhadepeca; // Variáveis para as peças e seleção da peça
+    torre = 0, bispo = 0, rainha = 0; // Aqui apontei a posição inicial das peças para teste
+    int movimento_torre, movimento_bispo, movimento_rainha; // Variável para o número de casas a ser movimentada
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    printf("Vamos movimentar as peças de Xadrez !\n");
+    printf("Escolha qual peça movimentar:\n 1. Torre\n 2. Bispo\n 3.Rainha\n");
+    scanf("%d", &escolhadepeca);
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    switch (escolhadepeca) {
+        case 1:
+        printf("Agora escolha quantas casas irá se movimentar\n");
+        scanf("%d", &movimento_torre);
+        for (torre; torre < movimento_torre; torre++) {
+            printf("A Torre se movimentou para Direita\n");
+        } break;
+        case 2:
+        printf("Agora escolha quantas casas irá se movimentar\n");
+        scanf("%d", &movimento_bispo);
+        while (bispo < movimento_bispo) {
+        printf("Bispo se movimentou para Direita e Cima\n");
+        bispo++;
+        } break;
+        case 3:
+        printf("Agora escolha quantas casas irá se movimentar\n");
+        scanf("%d", &movimento_rainha);
+        do {
+            printf("A Rainha se movimentou para Esquerda\n");
+            rainha++;
+        } while (rainha < movimento_rainha);
+        break;
+        default:
+        printf("opção inválida !\n");
+        break;
+}
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
